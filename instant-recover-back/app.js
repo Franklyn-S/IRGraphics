c3ae737f8aefc55b3e5e19f1be65d9132b97e3dc
Franklyn Seabra <franklyn.seabra@osf.digital>
2021-11-01 15:15:42 -0300

    initial working service to update config file

--- instant-recover-back/app.js
+++ instant-recover-back/app.js
@@ -1,12 +1,38 @@
-const express = require("express");
+const fs = require("fs");
 
-const processLineByLine = require("./processLineByLine");
+const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("teste");
-  processLineByLine();
+app.use(express.json());
+
+app.post("/update-config", (req, res, next) => {
+  const filePath = "test.conf";
+  const configs = req.body;
+  const keys = Object.keys(configs);
+  try {
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Não foi possível salvar as alterações.");
+  }
+  const file = fs.createWriteStream(filePath);
+  file.on("error", (err) => {
+    console.log(err);
+    res.status(500).send("Erro! As alterações não foram salvas.");
+  });
+  keys.forEach((key) => {
+    const value =
+      typeof configs[key] === "string" ? `"${configs[key]}"` : configs[key];
+    if (value !== "") {
+      file.write(`${key} = ${value};\n`);
+    }
+  });
+  file.end();
+
+  res.status(200).send("Configurações atualizadas!");
 });
 
 app.listen(5000);
